2cb04504be0a393cfc1b7f579709888e647f6fad
SkySmith2 <daniel.mulley@Outlook.com>
2019-02-02 19:36:57 +0000

    Did some fix shit for serverlink

--- src/main/java/uk/noxiousbot/NoxiousBot/Main.java
+++ src/main/java/uk/noxiousbot/NoxiousBot/Main.java
@@ -2,13 +2,18 @@ package uk.noxiousbot.NoxiousBot;
 
 import sx.blah.discord.api.ClientBuilder;
 import sx.blah.discord.api.IDiscordClient;
+import uk.noxiousbot.NoxiousBot.Commands.MessageListener;
+
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
         String token = "NTMwNzY4ODQ5NTk1NzkzNDE5.Dzcuew.WTJtV2TCLjbaTAeDzwZIxWsY8oI";
         IDiscordClient cli = getBuiltDiscordClient(token);
+        cli.getDispatcher().registerListener(new MessageListener());
         cli.login();
-        ServerLink sl = new ServerLink(cli.getChannelByID(519601289626124299L));
+        try {TimeUnit.SECONDS.sleep(10);}catch (Exception e) {}
+        ServerLink sl = new ServerLink(cli.getChannelByID(470673417583067166L));
     }
 
     public static IDiscordClient getBuiltDiscordClient(String token) {
