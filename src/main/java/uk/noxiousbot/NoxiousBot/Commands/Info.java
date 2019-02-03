6fb5da863f72286ef017348bad3458abc4a49d44
SkySmith2 <daniel.mulley@Outlook.com>
2019-02-03 20:26:18 +0000

    Added info command

---
+++ src/main/java/uk/noxiousbot/NoxiousBot/Commands/Info.java
@@ -0,0 +1,25 @@
+package uk.noxiousbot.NoxiousBot.Commands;
+
+import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
+import sx.blah.discord.util.EmbedBuilder;
+import sx.blah.discord.util.RequestBuffer;
+
+public class Info extends CommandCore{
+    public Info () {
+        commandName = "Info";
+        helpMessage = "Just shows some information about the bot";
+        Usage = "?info";
+    }
+
+    @Override
+    public boolean executeCommand(MessageReceivedEvent event, String[] argArray) {
+        EmbedBuilder builder = new EmbedBuilder();
+        builder.withAuthorName("Information");
+        builder.withDescription("This bot was made for Gladius Gaming as a little fun thing, the whole point is to become the highest ranking, the ordinal" +
+                ". Becoming the ordinal basically plasters your name everywhere but who doesn't want that. Gain coins by playing minigames, talking in text and voice channels," +
+                "and also duelling other players for their coins. For more information use the ?help command");
+        builder.withFooterText("Made by Sky#2134");
+        RequestBuffer.request(() -> event.getMessage().getChannel().sendMessage(builder.build()));
+        return true;
+    }
+}
