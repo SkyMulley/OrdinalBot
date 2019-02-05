a00db0b6970ae596f745f04fc7224c8ba1625029
skysmith2 <daniel.mulley@outlook.com>
2019-02-05 22:07:42 +0000

    Updated Main to v3

---
+++ src/main/java/mulley/sky/OrdinalBot/Main.java
@@ -0,0 +1,20 @@
+package mulley.sky.OrdinalBot;
+
+import discord4j.core.DiscordClient;
+import discord4j.core.DiscordClientBuilder;
+import discord4j.core.event.domain.lifecycle.ReadyEvent;
+import discord4j.core.object.entity.TextChannel;
+import discord4j.core.object.util.Snowflake;
+
+public class Main {
+    public static void main(String[] args) {
+        DiscordClientBuilder builder = new DiscordClientBuilder("NTMwNzY4ODQ5NTk1NzkzNDE5.Dzcuew.WTJtV2TCLjbaTAeDzwZIxWsY8oI");
+        DiscordClient cli = builder.build();
+        cli.login();
+        cli.getEventDispatcher().on(ReadyEvent.class)
+                .subscribe(event -> {
+                    ServerLink sl = new ServerLink((TextChannel) cli.getChannelById(Snowflake.of(470673417583067166L)).block());
+                    System.out.println("Bot is online, logged in as "+event.getSelf().getUsername()+event.getSelf().getDiscriminator()+" and server link should be setup");
+                });
+    }
+}
